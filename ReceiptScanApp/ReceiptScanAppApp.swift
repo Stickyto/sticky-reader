a0b974131cfe18f82e3a102cd9257c57939e1388
James <j@jada.io>
2025-08-16 05:15:45 +0100

    Initial Commit

---
+++ ReceiptScanApp/ReceiptScanAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReceiptScanAppApp.swift
+//  ReceiptScanApp
+//
+//  Created by James on 16/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct ReceiptScanAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ReceiptScanAppTests/ReceiptScanAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ReceiptScanAppTests.swift
+//  ReceiptScanAppTests
+//
+//  Created by James on 16/08/2025.
+//
+
+import Testing
+@testable import ReceiptScanApp
+
+struct ReceiptScanAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
